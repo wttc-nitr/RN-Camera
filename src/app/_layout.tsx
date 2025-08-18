91fd0b67ffe938032b02696d80f4cd7b1922487d
Animesh Kumar <kumaranimesh097@gmail.com>
2025-08-18 19:17:55 +0530

    create layout file & use Stack Navigator

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Slot, Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{ headerTintColor: "blue", headerTitleAlign: "center" }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
+}
